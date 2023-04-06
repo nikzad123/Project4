30b6dba1b2bb8bc293d2c67c0afcf0a879f42468
mohammadpanahnikzad <mohammadpanahnikzad@localhost>
2023-04-06 00:03:01 -0400

    Test 4

---
+++ src/gitHubProject/GitProject1.java
@@ -0,0 +1,13 @@
+package gitHubProject;
+
+public class GitProject1 {
+	
+	public static void main(String args []) {
+		
+		System.out.println("First Project of GitHub");
+		
+		System.out.println("Test...");
+		
+	}
+
+}
